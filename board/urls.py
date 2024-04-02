0262d8b6895bb744ef7905a03efd6fe551326b9d
杨慎 <2022012073@admin.secoder.net>
2024-04-02 08:39:08 +0000

    #1 hotfix board/urls.py: add api/login urlpattern

--- board/urls.py
+++ board/urls.py
@@ -4,6 +4,7 @@ import board.views as views
 urlpatterns = [
     path('startup', views.startup),
     path('login', views.login),
+    path('api/login', views.login),
     path('boards', views.boards),
     # TODO Start: [Student] add routing paths for `boards/<index>` and `user/<userName>`
     path('boards/<index>', views.boards_index),
