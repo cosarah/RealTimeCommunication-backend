a8a199eafdfaa9fbee072ee3b2f924c0c0042063
梁梓宸 <2022010800@secoder.net>
2024-04-18 18:09:14 +0800

    (#19)fix start.sh:use LF

--- check.sh
+++ check.sh
@@ -2,5 +2,5 @@ docker login registry.secoder.net
 # Username 2022010800
 # Password liang070800
 # 应该会显示 Login Succeeded
-docker pull registry.secoder.net/xxx/yyy:master # 把 xxx/yyy 换成本组仓库名，就是你要测试正不正常的那个仓库，下同
+docker pull registry.secoder.net/allright/app-backend:dev # 把 xxx/yyy 换成本组仓库名，就是你要测试正不正常的那个仓库，下同
 docker run -it --rm -p 9090:80 registry.secoder.net/allright/app-backend:dev 
