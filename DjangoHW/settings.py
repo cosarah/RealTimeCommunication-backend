a0e05d4d264f1507ab6f51a5d125fe878407e7f9
cosarah <1322568757@qq.com>
2024-05-03 15:34:36 +0800

    (#7)build settings.py: add localhost to whitelist

--- DjangoHW/settings.py
+++ DjangoHW/settings.py
@@ -63,7 +63,8 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = [
     'https://app-frontend-allright.app.secoder.net',
-    'https://frontend-dev-allright.app.secoder.net'
+    'https://frontend-dev-allright.app.secoder.net',
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'DjangoHW.urls'
