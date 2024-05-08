023394d10ebb31433ca9214bf97fce7ca5db3809
梁梓宸 <2022010800@secoder.net>
2024-05-08 22:29:19 +0800

    (#19)config dockerfile:mkdir db

--- DjangoHW/settings.py
+++ DjangoHW/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME':  Path(DATABASE_FOLDER) / 'db.sqlite3',
         'TEST': {
-            'NAME': Path(DATABASE_FOLDER) / 'test_db.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3'
         }
     }  # Change to MySQL or other databases for your FINAL project
 }
